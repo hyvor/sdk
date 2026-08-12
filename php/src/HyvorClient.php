357da53c9434766955aeda6387af675df4aa9af7
Supun Wimalasena <supun@hyvor.com>
2026-08-12 23:43:17 +0200

    serializer

--- php/src/HyvorClient.php
+++ php/src/HyvorClient.php
@@ -9,6 +9,7 @@ use Hyvor\Sdk\Auth\TokenProviderInterface;
 use Hyvor\Sdk\Http\ProductBaseUrl;
 use Hyvor\Sdk\Http\Transport;
 use Hyvor\Sdk\Post\PostClient;
+use Hyvor\Sdk\Serialization\SerializerFactory;
 use Hyvor\Sdk\Talk\TalkClient;
 use Http\Discovery\Psr17FactoryDiscovery;
 use Http\Discovery\Psr18ClientDiscovery;
@@ -71,6 +72,8 @@ final class HyvorClient
             );
         }
 
+        $serializer = SerializerFactory::create();
+
         $transport = fn(string $product) => new Transport(
             httpClient: $httpClient,
             requestFactory: $requestFactory,
@@ -81,6 +84,7 @@ final class HyvorClient
             defaultRetryMaxAttempts: $retryMaxAttempts,
             defaultRetryBackoffFactor: $retryBackoffFactor,
             userAgent: 'hyvor/sdk-php/' . Version::VERSION,
+            serializer: $serializer,
         );
 
         $this->talk = new TalkClient($transport('talk'));
